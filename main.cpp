5bcaf0b74e803d6e3ede268a2be473145306831a
danivdwerf@gmail.com <danivdwerf@gmail.com>
2017-12-22 10:10:20 +0100

    renamed callback to something more appropriate

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "includes/Window.h"
 // #include "includes/Menu.mm"
 
-void showAss()
+void callback()
 {
     std::cout << "Nice ass boi" << '\n';
 }
@@ -10,7 +10,7 @@ void showAss()
 int main(int argc, char* argv[])
 {
   Window* window = new Window("Hoppakee", 0, 0, 640, 360);
-  // Menu* assMenu = new Menu("STOP!", showAss, "a");
+  // Menu* assMenu = new Menu("STOP!", callback, "a");
   // window->addMenu(assMenu);
   window->run();
   return 0;
