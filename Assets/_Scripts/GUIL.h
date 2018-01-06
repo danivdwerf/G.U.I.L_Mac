4490c9d68d5e5ef5e7a2c8d364155f624261f729
danivdwerf <danivdwerf@gmail.com>
2018-01-06 11:51:46 +0100

    put includes in one file

---
+++ Assets/_Scripts/GUIL.h
@@ -0,0 +1,9 @@
+#ifndef GUIL_H
+#define GUIL_H
+#include "CPP/Window.h"
+#include "CPP/Menu.h"
+#include "CPP/MenuItem.h"
+#include "CPP/MainMenu.h"
+#include "CPP/GRect.h"
+#include "CPP/Button.h"
+#endif
