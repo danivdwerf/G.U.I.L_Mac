2689cfbfce03566f21456b60900ae854b9aad042
danivdwerf <danivdwerf@gmail.com>
2018-01-09 20:50:59 +0100

    Created getter for the button's type, made button with image work for multiple types of button

--- Assets/_Scripts/OBJC/OBJCButton.m
+++ Assets/_Scripts/OBJC/OBJCButton.m
@@ -1,75 +1,72 @@
 @interface OBJCButton : NSObject
 {
   NSButton* buttonObject;
+  int8_t type;
   void(*callback)();
 }
 -(id)initWithTitle:(const char*)t xPos:(int)x yPos:(int)y width:(int)w height:(int)h type:(int8_t)type callback:(void(*)())c;
 -(id)initWithTitle:(const char*)t frame:(NSRect)r type:(int8_t)type callback:(void(*)())c;
--(id)initWithImagePath:(const char*)i xPos:(int)x yPos:(int)y callback:(void(*)())c;
+-(id)initWithImagePath:(const char*)i xPos:(int)x yPos:(int)y type:(int8_t)t callback:(void(*)())c;
+
 -(void)onClick:(id)sender;
 -(void)show:(NSWindow*)window;
+
+-(int8_t)Type;
 @end
 
 @implementation OBJCButton
--(id)initWithTitle:(const char*)t xPos:(int)x yPos:(int)y width:(int)w height:(int)h type:(int8_t)type callback:(void(*)())c
+-(id)initWithTitle:(const char*)t xPos:(int)x yPos:(int)y width:(int)w height:(int)h type:(int8_t)buttonType callback:(void(*)())c
 {
   if(!(self = [super init]))
     return nil;
+
+  self->type = buttonType;
 
   NSRect frame = NSMakeRect(x, y, w, h);
   self->buttonObject = [[[NSButton alloc] initWithFrame: frame] autorelease];
   [self->buttonObject setTitle: [NSString stringWithUTF8String:t]];
   [self->buttonObject setTarget: self];
-  [self->buttonObject setButtonType:type];
+  [self->buttonObject setButtonType:self->type];
   [self->buttonObject setAction:@selector(onClick:)];
   self->callback = c;
+
 
   return self;
 }
 
--(id)initWithTitle:(const char*)t frame:(NSRect)r type:(int8_t)type callback:(void(*)())c
+-(id)initWithTitle:(const char*)t frame:(NSRect)r type:(int8_t)buttonType callback:(void(*)())c
 {
   if(!(self = [super init]))
     return nil;
+
+  self->type = buttonType;
 
   self->buttonObject = [[[NSButton alloc] initWithFrame:r] autorelease];
   [self->buttonObject setTitle: [NSString stringWithUTF8String:t]];
   [self->buttonObject setTarget: self];
-  [self->buttonObject setButtonType:type];
+  [self->buttonObject setButtonType: self->type];
   [self->buttonObject setAction:@selector(onClick:)];
   self->callback = c;
 
   return self;
 }
 
--(id)initWithImagePath:(const char*)i xPos:(int)x yPos:(int)y callback:(void(*)())c
+-(id)initWithImagePath:(const char*)i xPos:(int)x yPos:(int)y type:(int8_t)buttonType callback:(void(*)())c
 {
   if(!(self = [super init]))
     return nil;
 
+  self->type = buttonType;
   NSImage* image = [[NSImage alloc]initWithContentsOfFile:[NSString stringWithUTF8String: i]];
   NSRect frame = NSMakeRect(x, y, (int)[image size].width, (int)[image size].height);
   self->buttonObject = [[[NSButton alloc] initWithFrame: frame] autorelease];
   [self->buttonObject setTarget: self];
   [self->buttonObject setAction:@selector(onClick:)];
+  [self->buttonObject setTitle:@""];
   [self->buttonObject setImage: image];
+  [self->buttonObject setButtonType: self->type];
   [self->buttonObject setBordered:NO];
   self->callback = c;
-  return self;
-}
-
--(id)checkboxWithTitle:(const char*)t xPos:(int)x yPos:(int)y width:(int)w height:(int)h
-{
-  if(!(self = [super init]))
-    return nil;
-
-  NSRect frame = NSMakeRect(x, y, w, h);
-  self->buttonObject = [[[NSButton alloc] initWithFrame:frame] autorelease];
-  [self->buttonObject setButtonType:NSSwitchButton];
-  [self->buttonObject setTitle:[NSString stringWithUTF8String:t]];
-  [self->buttonObject setTarget: self];
-  [self->buttonObject setAction:@selector(onClick:)];
-  // self->callback = c;
 
   return self;
 }
@@ -83,5 +80,10 @@
 {
   if(callback != NULL)
     self->callback();
+}
+
+-(int8_t)Type
+{
+    return self->type;
 }
 @end
